cb5dca5d79b8385d06156d01476329bebb2978fd
Nikki <nikki@Nikkis-MacBook-Air-M4.local>
2026-09-09 10:38:18 +1000

    Added analysis

--- my_analysis.R
+++ my_analysis.R
@@ -3,4 +3,11 @@ my_vector <- 1:100
 mean(my_vector)
 hist(my_vector)
 
+#type soem stuff 
+median(my_vector)
+
 # Need to always make sure to save the script, or else it wont work with git 
+
+# Tick the boxes (Add turns green)
+# Git commit step by pressing the "Commit" button which 
+# Good commit messages are normally descriptive 
